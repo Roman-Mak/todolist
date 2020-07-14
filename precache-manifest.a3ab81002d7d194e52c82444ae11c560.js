9f7cfc245be1d6cc3cbfbffbe75b5d2024164b42
Roman-Mak <meller1758@gmail.com>
2020-07-14 22:56:43 +0300

    Updates

---
+++ precache-manifest.a3ab81002d7d194e52c82444ae11c560.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1dcb5e75f956b807e4749348db17774",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "c69ecad8be7f64e7fc85",
+    "url": "/todolist/static/css/main.7c1e4973.chunk.css"
+  },
+  {
+    "revision": "f0e9f274a50d4c47c150",
+    "url": "/todolist/static/js/2.f02ec5ef.chunk.js"
+  },
+  {
+    "revision": "c69ecad8be7f64e7fc85",
+    "url": "/todolist/static/js/main.9f7fb5af.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  },
+  {
+    "revision": "986e54dcc7df5a8e0a71297f0b51816f",
+    "url": "/todolist/static/media/tasks-preloader.986e54dc.svg"
+  },
+  {
+    "revision": "a67153fdc30773e3332884ce3118cdc0",
+    "url": "/todolist/static/media/todolists-preloader.a67153fd.svg"
+  }
+]);
